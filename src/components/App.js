725628a553f6d2698a6c8640a326d33dcc2bcef9
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-01-02 15:24:29 +0100

    Added a mutation and a component to create links

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <div>
+        <LinkList />
+        <CreateLink />
+      </div>
     );
   }
 }
