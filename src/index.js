e77520c735e14d6cb06c0ba99667c41b9f50b222
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-01-02 14:02:06 +0100

    Scaffold the project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjbxmlgfg1fn50165jk1sqxs2' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'));
+
+registerServiceWorker();
