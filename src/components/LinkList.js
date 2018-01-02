b4766d201a198f14d5d398085eaf4849851cbc4a
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-01-02 14:11:11 +0100

    First query

---
+++ src/components/LinkList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import Link from './Link'
+
+class LinkList extends Component {
+  render() {
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>
+    }
+
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = this.props.allLinksQuery.allLinks
+
+    return (
+      <div>
+        {linksToRender.map(link => (
+          <Link key={link.id} link={link} />
+        ))}
+      </div>
+    )
+  }
+}
+
+const ALL_LINKS_QUERY = gql`
+query AllLinksQuery {
+  allLinks {
+    id
+    createdAt
+    url
+    description
+  }
+}
+`
+
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
